1e574920ac4b422fb8aef9f00b0bbc583abfc5b0
Joris Dirickx <joris.dirickx@outlook.com>
2015-10-14 21:16:37 +0200

    Intitial check in

---
+++ HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HouseOfTheFuture.MobileApp.WinPhone.Resources;
+
+namespace HouseOfTheFuture.MobileApp.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
