48b28c24eee7dc21ee5fc1f7ad383bb7cf803256
Kevin De Bruyn <Kevin.DeBruyn@infosupport.com>
2015-10-24 10:58:11 +0200

    Xamarin Forms upgrade + Windows Phone & Xamarin Forms

--- HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp.Droid/MainActivity.cs
+++ HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp.Droid/MainActivity.cs
@@ -10,13 +10,13 @@ using Android.OS;
 namespace HouseOfTheFuture.MobileApp.Droid
 {
     [Activity(Label = "HouseOfTheFuture.MobileApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
---
+++ HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HouseOfTheFuture.MobileApp.Views;
+using Xamarin.Forms;
+
+namespace HouseOfTheFuture.MobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            this.MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+    }
+}
