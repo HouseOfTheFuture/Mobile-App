49e787d45c3cae40b6f2d16f148d1ce93d3bffa4
Kevin De Bruyn <Kevin.DeBruyn@infosupport.com>
2015-11-10 13:57:48 +0100

    Sockets reworked

---
+++ HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp/HouseOfTheFuture.MobileApp.Sockets/DeviceFoundEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseOfTheFuture.MobileApp.Sockets
+{
+    public class DeviceFoundEventArgs : EventArgs
+    {
+        public IDevice Device { get; set; }
+    }
+}
