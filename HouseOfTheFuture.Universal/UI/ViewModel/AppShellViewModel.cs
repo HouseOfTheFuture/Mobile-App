8acb87d139ff045cfed285b21f39f94c338702e9
YoeriVD <yoeri.vandamme@gmail.com>
2015-11-10 12:00:55 +0100

    changed the opening and closing implementation to mvvm

--- HouseOfTheFuture.Universal/UI/ViewModel/AppShellViewModel.cs
+++ HouseOfTheFuture.Universal/UI/ViewModel/AppShellViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using InfoSupport.TickTack.App.Views;
 
 namespace InfoSupport.TickTack.App.ViewModel
@@ -7,6 +9,20 @@ namespace InfoSupport.TickTack.App.ViewModel
     public class AppShellViewModel : ViewModelBase
     {
         public ObservableCollection<MenuItem> Menu { get; } = new ObservableCollection<MenuItem>();
+
+        private bool _isPaneOpen;
+
+        public bool IsPaneOpen
+        {
+            get { return _isPaneOpen; }
+            set
+            {
+                _isPaneOpen = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand ShowMenu => new RelayCommand(() => IsPaneOpen = !IsPaneOpen);
 
         public AppShellViewModel()
         {
